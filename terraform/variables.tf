0bcc0e53db39d818b121c798f0df76751c71d95c
Roman Malter <roman.malter@gmail.com>
2026-06-21 20:57:26 -0500

    Add AWS region and instance type variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,11 @@
+variable "aws_region" {
+  description = "The AWS region to deploy infrastructure into"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "instance_type" {
+  description = "The virtual hardware size of our server"
+  type        = string
+  default     = "t2.micro" # Free-tier eligible size
+}
